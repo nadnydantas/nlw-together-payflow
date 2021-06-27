6b45e31be57564549b1d822bc4c1a312059212b7
Nadny Dantas <nadny.dantas@gmail.com>
2021-06-27 19:17:02 -0400

    Animações no input_text e no boleto_tile

--- lib/shared/widgets/input_text/input_text_widget.dart
+++ lib/shared/widgets/input_text/input_text_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -27,47 +28,50 @@ class InputTextWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     // Para entrada de dados do usuário, há o TextField e o TextFormField
     // O TextFormField já vem com propriedade de validação das infos
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: Column(
-        children: [
-          TextFormField(
-            controller: controller,
-            onChanged: onChanged,
-            initialValue: initialValue,
-            validator: validator,
-            style: TextStyles.input,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.zero,
-              labelText: label,
-              labelStyle: TextStyles.input,
-              icon: Row(
-                // colocar a row no tamanho minimo porque o texto tinha ido para o final
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 18),
-                    child: Icon(
-                      icon,
-                      color: AppColors.primary,
+    return AnimatedCard(
+      direction: AnimatedCardDirection.left,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 16),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller,
+              onChanged: onChanged,
+              initialValue: initialValue,
+              validator: validator,
+              style: TextStyles.input,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                labelText: label,
+                labelStyle: TextStyles.input,
+                icon: Row(
+                  // colocar a row no tamanho minimo porque o texto tinha ido para o final
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 18),
+                      child: Icon(
+                        icon,
+                        color: AppColors.primary,
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: 1,
-                    height: 48,
-                    color: AppColors.stroke,
-                  ),
-                ],
+                    Container(
+                      width: 1,
+                      height: 48,
+                      color: AppColors.stroke,
+                    ),
+                  ],
+                ),
+                border: InputBorder.none,
               ),
-              border: InputBorder.none,
             ),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-            color: AppColors.stroke,
-          ),
-        ],
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: AppColors.stroke,
+            ),
+          ],
+        ),
       ),
     );
   }
